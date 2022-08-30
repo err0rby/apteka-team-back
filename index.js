1a003bc53fab46a4ce3f5077507ca1d7de6b5ab8
Saluev <ibragim.saluev@mail.ru>
2022-08-30 12:29:27 +0300

    Добавил лекарство

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const path = require('path');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
 const start = async () => {
-    try {
-        await mongoose.connect(process.env.MONGOSER)
-        console.log('Подключились к базе')
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGOSER);
+    console.log("Подключились к базе");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 app.listen(process.env.PORT, () => {
-    console.log('Подключились к серверу')
-})
+  console.log("Подключились к серверу");
+});
 
 start();
