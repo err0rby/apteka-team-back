f0ea2337298f25db5bfcfef55e14bfbac2fd8c49
errorby <mdidayev@inbox.ru>
2022-08-30 15:32:21 +0300

    Токен и хэш

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,4 +1,7 @@
 const User = require('../models/User.model');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 module.exports.userController = {
     addUser: async (req, res) => {
         try {
