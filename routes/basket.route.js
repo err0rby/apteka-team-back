6c65200e261961928fe33691ad51267e0a7c36f3
GooA-NA <nasirgudesev@gmail.com>
2022-08-31 11:42:33 +0300

    BasketRoute

--- routes/basket.route.js
+++ routes/basket.route.js
@@ -2,7 +2,6 @@ const { Router } = require('express')
 const { basketController } = require('../controllers/basket.controller')
 const router = Router()
 
-router.post('/basket', basketController.postBasket)
 router.get('/basket', basketController.getBaskets)
 
 module.exports = router
