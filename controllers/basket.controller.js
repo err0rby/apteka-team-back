5d1d2f02fbfdbe658b0c492405ab7cd14a6f3334
GooA-NA <nasirgudesev@gmail.com>
2022-09-01 11:58:46 +0300

    Сделал немного карзину

--- controllers/basket.controller.js
+++ controllers/basket.controller.js
@@ -2,8 +2,26 @@ const Basket = require("../models/Basket.model")
 
 module.exports.basketController = {
     getBaskets: async (req, res) => {
-        const allBaskets = await Basket.find()
+        const allBaskets = await Basket.find().populate('drugs')
         res.json(allBaskets)
     },
+    getBasketById: async (req, res) => {
+        try {
+            const basket = await Basket.find({user: req.body.userId}).populate('drugs')
+            res.json(basket)
+        } catch (e) {
+            return res.status(401).json(e.toString())
+        }
+    },
+    patchBasket: async (req, res) => {
+        try {
+            const newBasket = await Basket.findByIdAndUpdate(req.body.basketId, {
+                $push: {drugs: req.body.drugId}
+            }).populate('drugs')
+            res.json(newBasket)
+        } catch (e) {
+            return res.status(401).json(e.toString())
+        }
+    }
 
 }
--- routes/basket.route.js
+++ routes/basket.route.js
@@ -1,7 +1,11 @@
 const { Router } = require('express')
 const { basketController } = require('../controllers/basket.controller')
+const authMiddleware = require('../middleware/auth.middleware')
 const router = Router()
 
 router.get('/basket', basketController.getBaskets)
+router.post('/basket/id', basketController.getBasketById)
+router.patch('/basket', basketController.patchBasket)
+
 
 module.exports = router
