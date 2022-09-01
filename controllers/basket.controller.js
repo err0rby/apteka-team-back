56cbbb7c1d72a7cb85c5ef9deb02300ec5b67f27
GooA-NA <nasirgudesev@gmail.com>
2022-09-01 16:26:59 +0300

    Commit na back

--- controllers/basket.controller.js
+++ controllers/basket.controller.js
@@ -8,6 +8,16 @@ module.exports.basketController = {
         } catch (e) {
             res.status(401).json(e.toString())
         }
+    },
+    pathBasket: async (req, res) => {
+        try {
+            const basket = await User.findByIdAndUpdate(req.body.userId, {
+                $push: {basket: req.body.drug}
+            }).populate('basket')
+            res.json(basket)
+        } catch (e) {
+            res.status(401).json(e.toString())
+        }
     }
 
 }
--- routes/basket.route.js
+++ routes/basket.route.js
@@ -4,6 +4,8 @@ const authMiddleware = require('../middleware/auth.middleware')
 const router = Router()
 
 router.post('/basket', basketController.getBasket)
+router.patch('/basket', basketController.pathBasket)
+
 
 
 module.exports = router
